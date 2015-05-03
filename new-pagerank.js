2b3f22717b6fc12df11c691c9012988e236fec03
Ian Sibner <sibnerian@gmail.com>
2015-05-03 19:59:02 -0400

    foo

--- new-pagerank.js
+++ new-pagerank.js
@@ -3,6 +3,10 @@ var hl = require('highland');
 var cheerio = require('cheerio');
 var crypto = require('crypto');
 var sprom = require('sprom');
+var async = require('async');
+var fs = require('fs');
+
+var FILENAME = 'pageranks-with-descriptions';
 
 var sha1 = function(input){
     return crypto.createHash('sha1').update(input).digest('hex');
@@ -11,22 +15,42 @@ var sha1 = function(input){
 var s3 = new AWS.S3();
 var pageRankParams = {Bucket: 'cis555-bucket', Key: 'pagerank-out/part-r-00000'};
 
-var file = require('fs').createWriteStream('pageranks-with-descriptions');
+fs.truncateSync(FILENAME, 0);
 
-hl(s3.getObject(pageRankParams).createReadStream()).split().each(function (line) {
-  var urlPageRank = line.split('\t');
-  var filename = sha1(urlPageRank[0]);
-  var htmlParams = {Bucket: 'cis555-bucket', Key: 'crawl/' + filename};
-  sprom(s3.getObject(htmlParams).createReadStream()).then(function (bodyBuffer) {
-    var $ = cheerio.load(bodyBuffer.toString());
-    var title = $('head title').text().replace(/(\r\n|\n|\r)/gm,'').trim();
-    var description = $('meta[name=description]').attr('content');
-    if (!description) {
-      description = 'No description available.'
-    } else {
-      description = description.replace(/(\r\n|\n|\r)/gm,'').trim();
+var idx = 0;
+var errors = 0;
+var worker = function (line, callback) {
+    idx++;
+    if (idx % 100 === 0) {
+      console.log('processed ' + idx + ' docs; ' + errors + ' errors so far')
     }
-    var delimiter = '\u2603\u2603\u2603';
-    file.write(urlPageRank[0] + delimiter + urlPageRank[1] + delimiter + title + delimiter + description + '\n');
-  });
+    var urlPageRank = line.split('\t');
+    var filename = sha1(urlPageRank[0]);
+    var htmlParams = {Bucket: 'cis555-bucket', Key: 'crawl-real/' + filename};
+    sprom(s3.getObject(htmlParams).createReadStream()).then(function (bodyBuffer) {
+      var $ = cheerio.load(bodyBuffer.toString());
+      var title = $('head title').text().replace(/(\r\n|\n|\r)/gm,'').trim();
+      var description = $('meta[name=description]').attr('content');
+      if (!description) {
+        description = 'No description available.'
+      } else {
+        description = description.replace(/(\r\n|\n|\r)/gm,'').trim();
+      }
+      var delimiter = '\u2603\u2603\u2603';
+      fs.appendFileSync(FILENAME, urlPageRank[0] + delimiter + urlPageRank[1] + delimiter + title + delimiter + description + '\n');
+      callback();
+    }, function (e) {
+      console.log(e.message + ' while processing ' + urlPageRank[0]);
+      errors++;
+      callback();
+    });
+};
+var q = async.queue(worker, 100);
+q.drain = function () {
+  console.log(idx + ' items processed with ' + errors + ' errors');
+};
+
+hl(s3.getObject(pageRankParams).createReadStream()).split().toArray(function (array) {
+  console.log('Starting with ' + array.length + ' items...');
+  q.push(array); 
 });
